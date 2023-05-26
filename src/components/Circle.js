328287f50aa3c9db0dc3318d3bfd9c03bb8dbbd8
EhsanKINUX <ehsankhwentao@gmail.com>
2023-05-26 16:47:20 +0330

    Creating Circle component

---
+++ src/components/Circle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Circle = (props) => {
+  return (
+    <div className='rounded-full absolute opacity-10 z-50' style={{...props}} />
+  )
+}
+
+export default Circle
