ff4f4137999a9208f188050d597bbf71e1097363
EhsanKINUX <ehsankhwentao@gmail.com>
2023-07-08 08:56:23 +0330

    Change config in order to deploy on github

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: {
+      // Set the output directory for the exported static files
+      dir: 'out',
+      // Specify the base path of your app (optional)
+      // basePath: '/your-base-path',
+    },
+    experimental: {
+      images: true
+    }
+  }
+  
+  module.exports = nextConfig
